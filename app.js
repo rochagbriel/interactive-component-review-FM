0979933e3c20189750c78f5e2a6ce075eac13b72
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-04 20:18:17 +0100

    forEach loop update

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 const reviewScreen = document.getElementById('review');
 const thankYou = document.getElementById('thankYou');
 const message = document.getElementById('message');
-
-let selectedButton1 = document.getElementById('button1');
-let selectedButton2 = document.getElementById('button2');
-let selectedButton3 = document.getElementById('button3');
-let selectedButton4 = document.getElementById('button4');
-let selectedButton5 = document.getElementById('button5');
+let ratingButtons = document.querySelectorAll('.button');
 
 const submit = document.getElementById('submit');
 let stars = 0;
 
-selectedButton1.addEventListener('click', () => {
-    return stars = 1;
-});
-selectedButton2.addEventListener('click', () => {
-    return stars = 2;
-});
-selectedButton3.addEventListener('click', () => {
-    return stars = 3;
-});
-selectedButton4.addEventListener('click', () => {
-    return stars = 4;
-});
-selectedButton5.addEventListener('click', () => {
-    return stars = 5;
-});
-
+ratingButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        return stars = index + 1;
+    })
+}); 
+    
 submit.addEventListener('click', () => {
     reviewScreen.style.display = 'none';
     thankYou.style.display = 'flex';

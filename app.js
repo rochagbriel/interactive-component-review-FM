69776a54ca02db93435a44fa3f277fd1ce312395
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-12 18:14:39 +0100

    new script

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const reviewScreen = document.getElementById('review');
 const thankYou = document.getElementById('thankYou');
 const message = document.getElementById('message');
-let ratingButtons = document.querySelectorAll('.button');
 
-const submit = document.getElementById('submit');
 let stars = 0;
 
-ratingButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        return stars = index + 1;
-    })
-}); 
-    
-submit.addEventListener('click', () => {
+document.getElementById("btnSubmit").onclick = function() {
+    var radios = document.getElementsByName("review");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            stars = radios[i].value;
+        }
+    } if (stars > 0) {
     reviewScreen.style.display = 'none';
     thankYou.style.display = 'flex';
     message.innerHTML = `You selected ${stars} out 5`;
-});
-
+    } else alert('Please select one option!')
+};
